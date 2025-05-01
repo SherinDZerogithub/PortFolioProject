b24693e85437a7d37d4cd64e883991529453d688
SherinDZerogithub <seriniharindya@gmail.com>
2025-05-01 13:57:55 +0530

    =================

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -33,11 +33,12 @@ export const Navbar = () => {
           <li>
             <a href="#skills">Skills</a>
           </li>
-          <li>
-            <a href="#experience">Experience</a>
-          </li>
+         
           <li>
             <a href="#projects">Projects</a>
+          </li>
+          <li>
+            <a href="#experience">Experience</a>
           </li>
           <li>
             <a href="#contact">Contact</a>
